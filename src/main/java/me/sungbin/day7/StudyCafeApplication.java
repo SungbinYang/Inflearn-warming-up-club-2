c40f524c9a696568f5162f5db1a336aa25116354
SungbinYang <ysb05222@naver.com>
2024-10-06 08:35:02 +0900

    예제 프로젝트 클론

---
+++ src/main/java/me/sungbin/day7/StudyCafeApplication.java
@@ -0,0 +1,10 @@
+package me.sungbin.day7;
+
+public class StudyCafeApplication {
+
+    public static void main(String[] args) {
+        StudyCafePassMachine studyCafePassMachine = new StudyCafePassMachine();
+        studyCafePassMachine.run();
+    }
+
+}
---
+++ src/main/java/me/sungbin/day7/exception/AppException.java
@@ -0,0 +1,9 @@
+package me.sungbin.day7.exception;
+
+public class AppException extends RuntimeException {
+
+    public AppException(String message) {
+        super(message);
+    }
+
+}
